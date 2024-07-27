bee54d5cee14a03b6ab4550b50d85c10baf252f3
googlepic0 <tkkaisha@gmail.com>
2024-07-27 15:06:44 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var password = "@27kaisha55@";
+function passcheck() {
+    if(document.getElementById('pass1').value != password){
+        alert('Wrong password, This is Not for You, Only for my Wife.Please Dont try again');
+        return false;
+    }
+    if(document.getElementById('pass1').value != password){
+        alert('Correct Password. Click Ok to enter webpage.');
+    }
+}
